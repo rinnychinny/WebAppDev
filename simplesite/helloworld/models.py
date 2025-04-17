263402dbc863cf0c582701c74a1f51a29598d60b
rinnychinny <richpemuk@gmail.com>
2025-04-17 21:17:18 +0200

    Working through views in lesson

--- simplesite/helloworld/models.py
+++ simplesite/helloworld/models.py
@@ -3,3 +3,15 @@ from django.db import models
 # Create your models here.
 class Hello(models.Model):
     text = models.CharField(max_length=200)
+
+class Person(models.Model):
+    name = models.CharField(max_length=500, null=False, blank=False, db_index=True)
+    age = models.IntegerField(null=False, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Address(models.Model):
+    number = models.IntegerField(null=False, blank=True)
+    street_name = models.CharField(max_length=500, null=False, blank=True)
+    resident = models.ForeignKey(Person, null=True, on_delete=models.SET_NULL)
--- simplesite/helloworld/views.py
+++ simplesite/helloworld/views.py
@@ -8,8 +8,16 @@ def index(request):
     return render(request, 'helloworld/index.html', {'data': response_string})
 # Create your views here.
 
-def simple_view(request):
+def simple_view_ip(request):
     header = request.META
     ip = header['REMOTE_ADDR']
     html = "<html><head></head><body>Your ip address is: "+ip+"</body></html>"
     return HttpResponse(html, content_type="text/html", status=200)
+
+def simple_view(request):
+    addresses = Address.objects.all()
+    first_address = addresses[0]
+    resident_name = str(first_address.resident)
+    html = "<html><head></head><bofdy>Name: "+resident_name+"<br />Address: "+first_address.street_name+"</body></html>"
+    return HttpResponse(html)
+
